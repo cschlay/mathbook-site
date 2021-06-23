5ae52367ce2aa00e3821735ccac675c4e2d30026
Chi-Hao Lay <chi-hao.lay@outlook.com>
2021-06-23 21:38:51 +0300

    add place holder attribution to every single .mdx page

--- src/components/MdxLayout.tsx
+++ src/components/MdxLayout.tsx
@@ -10,6 +10,28 @@ interface Props {
 
 const MdxLayout = ({ children }: Props) => (
     <MDXProvider components={{ Definition, Example, Tex: MathDisplay }}>
+        <small>
+            This project uses open source and public domain math textbook
+            materials. The included texts are{" "}
+            <a
+                href="https://openlogicproject.org/about/"
+                target="_blank"
+                rel="noreferrer"
+            >
+                Open Logic Project
+            </a>{" "}
+            (CC BY 4.0 License). The material is modified, combined with other
+            texts, and extended to fit computer science approach to mathematics.
+            Explicit references can be found in the{" "}
+            <a
+                href="https://github.com/cschlay/mathbook-site/tree/main/src/pages/book/chapters"
+                target="_blank"
+                rel="noreferrer"
+            >
+                source code
+            </a>
+            .
+        </small>
         {children}
     </MDXProvider>
 )
