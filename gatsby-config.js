ff3676e43c2b3d1a6354fbc51d67042d1213618a
Chi-Hao Lay <chi-hao.lay@outlook.com>
2021-06-23 21:44:38 +0300

    disable site map

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     },
     plugins: [
         "gatsby-plugin-react-helmet",
-        "gatsby-plugin-sitemap",
+        //"gatsby-plugin-sitemap",
         {
             resolve: "gatsby-plugin-manifest",
             options: {
