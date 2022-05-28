0d69dce011a2564d888ed7cda8b90e4757a5dbeb
Chi-Hao Lay <chi-hao.lay@outlook.com>
2022-05-28 12:21:01 +0300

    Improve SWR to not fetch if not authenticated

--- src/components/layouts/LayoutBase.tsx
+++ src/components/layouts/LayoutBase.tsx
@@ -1,39 +1,18 @@
-import * as React from "react";
+import { ReactNode } from "react";
 import { Footer } from "../Footer";
 import { Header } from "../Header";
-import { SWRConfig } from "swr";
-import { env } from "../../../env";
 
 interface Props {
-  children: React.ReactNode;
+  children: ReactNode;
 }
 export const LayoutBase = ({ children }: Props): JSX.Element => {
   return (
-    <SWRConfig
-      value={{
-        fetcher: swrFetch,
-        errorRetryCount: 0,
-      }}
-    >
+    <>
       <main>
         <Header />
         {children}
       </main>
       <Footer />
-    </SWRConfig>
+    </>
   );
-};
-
-const swrFetch = (url: string, init: object) => {
-  return fetch(`${env.API_HOST}${url}`, {
-    ...init,
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem("AccessToken")}`,
-    },
-  }).then((res) => {
-    if (res.ok) {
-      return res.json();
-    }
-    return Promise.reject(new Error());
-  });
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,40 @@ import "katex/dist/katex.min.css";
 import "../../styles/normalize.css";
 import "../../styles/highlight-theme.css";
 import "../../styles/base.css";
+import { env } from "../../env";
+import { SWRConfig } from "swr";
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <SWRConfig
+      value={{
+        fetcher: swrFetch,
+        errorRetryCount: 0,
+      }}
+    >
+      <Component {...pageProps} />
+    </SWRConfig>
+  );
+};
+
+const swrFetch = (url: string, init: object): Promise<unknown> => {
+  const accessToken = localStorage.getItem("AccessToken");
+
+  if (!accessToken) {
+    return Promise.reject(new Error());
+  }
+
+  return fetch(`${env.API_HOST}${url}`, {
+    ...init,
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(new Error());
+  });
 };
 
 export default App;
